dff58de54c6cd68b52747df3c7cbc9d8785dc2f8
Leevey <fsx0731@gmail.com>
2016-07-17 17:00:25 +0800

    prevent Fragment destroy from viewPager

--- app/src/main/java/com/ileevey/lazyload/SimpleFragmentPagerAdapter.java
+++ app/src/main/java/com/ileevey/lazyload/SimpleFragmentPagerAdapter.java
@@ -3,20 +3,21 @@ package com.ileevey.lazyload;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SimpleFragmentPagerAdapter  extends FragmentPagerAdapter {
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
     private List<Fragment> listFragments;
     private List<String> mTitleList = new ArrayList<>();//页卡标题集合
 
     public SimpleFragmentPagerAdapter(FragmentManager fm,
-                                  List<Fragment> al,
-                                  List<String> titleList) {
+                                      List<Fragment> al,
+                                      List<String> titleList) {
         super(fm);
         listFragments = al;
-        mTitleList =  titleList;
+        mTitleList = titleList;
     }
 
     public SimpleFragmentPagerAdapter(FragmentManager fm) {
@@ -31,6 +32,19 @@ public class SimpleFragmentPagerAdapter  extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return listFragments.size();
+    }
+
+    /**
+     * 防止频繁的销毁视图，设置ViewPager.setOffscreenPageLimit(2)<br/>
+     * 或者重写PagerAdaper的destroyItem方法为空,<br/>
+     * 即注释掉调用父类方法即可
+     * @param container
+     * @param position
+     * @param object
+     */
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        //super.destroyItem(container, position, object);
     }
 
     @Override
